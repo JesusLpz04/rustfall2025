17bf3c7e0e6298cb7a5b4c06494c6e3c87b266b7
JesusLpz04 <115657809+JesusLpz04@users.noreply.github.com>
2025-09-17 15:26:29 +0000

    Homework 1

---
+++ homework_1/src/main.rs
@@ -0,0 +1,13 @@
+fn assignment1(){
+    //Temp converter
+    fn fahrenheit_to_celsius(x:i32) -> i32{
+        return (x-32)(5/9)
+    }
+    fn celsius_to_fahrenheit(x:i32) -> i32{
+        return x(9/5)+32
+    }
+
+}
+fn main() {
+    assignment1();
+}
